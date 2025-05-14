990f081b7d9686a3b1ef1ff205c093da33200df3
Muhammed Ahmad <99442871+Muhammed-L-Ahmad@users.noreply.github.com>
2025-05-14 19:58:47 +0000

    I tried to fix all the typos and missed errors in app rb, homepage erb and results erb.

--- app.rb
+++ app.rb
@@ -1,19 +1,19 @@
 require "sinatra"
-require "sinatra/reloader""
+require "sinatra/reloader"
 
 get("/") do
   erb(:homepage)
 end
 
 get("/process_roll") do
-  @num_dice = params.fetch("dice).to_i
+  @num_dice = params.fetch("dice").to_i
 
   @num_sides = params.fetch("sides").to_i
 
   @rolls = []
 
   @num_dice.times do
-    @rolls.push(rand(1..num_sides))
+    @rolls.push(rand(1..@num_sides))
   end
 
   erb(:results)
